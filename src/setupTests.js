34502c7e3004321e677c1bd8a57332de9d8f4aad
Jhonatan M <jhonatan.mendez664@pascualbravo.edu.co>
2025-05-20 18:02:41 -0500

    Add license in js and jsx docs

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,6 @@
+// Copyright 2025 Anti-Patrones
+// This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+// http://creativecommons.org/licenses/by-sa/4.0/
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
